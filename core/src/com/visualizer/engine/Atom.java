e493910676cd5173763685d5c57543f991faeb71
rzymianin4 <dave.x00@gmail.com>
2017-07-20 17:48:32 +0200

    gdx in swing subframe

---
+++ core/src/com/visualizer/engine/Atom.java
@@ -0,0 +1,45 @@
+package com.visualizer.engine;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by dawid on 04.04.17.
+ */
+public class Atom {
+    BlendingAttribute blendingAttribute;
+    Model atom;
+    ModelInstance instance;
+
+    public Atom(ModelBuilder builder, float w, float h, float d, Matrix4 transform, Color color, int divisions) {
+        this.blendingAttribute = new BlendingAttribute(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA, 1);
+        this.atom = builder.createSphere(w, h, d, divisions, divisions,
+                new Material(ColorAttribute.createDiffuse(color), blendingAttribute),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal );
+        this.instance = new ModelInstance(atom, transform); }
+
+    public void changeOpacity(float opacity) {
+        instance.materials.get(0).set(new BlendingAttribute(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA, opacity)); }
+
+    public void changeColor(Color color) {
+        instance.materials.get(0).set(ColorAttribute.createDiffuse(color)); }
+
+    public void scale(float factor) {
+        instance.transform.scl(factor); }
+
+    public void translate(float factor) {
+        Vector3 position = instance.transform.getTranslation(Vector3.Zero);
+        instance.transform.setTranslation(position.scl(factor)); }
+
+    public ModelInstance getInstance() {
+        return instance; }
+}
---
+++ core/src/com/visualizer/engine/Main.java
@@ -0,0 +1,80 @@
+package com.visualizer.engine;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.environment.PointLight;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.*;
+
+public class Main implements ApplicationListener {
+	private Environment environment;
+	private Viewport viewport;
+	
+	private Controller controller;
+	
+	private AllModels modelBatch;
+	//private ModelBuilder modelBuilder;
+	
+	@Override
+	public void create () {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.5f, 0.5f, 0.5f, 1f));
+		environment.add(new PointLight().set(0.8f, 0.8f, 0.8f, -10f, -10f, 10f, 200f));
+		
+		//modelBuilder = new ModelBuilder();
+		//modelBatch = new ModelBatch();
+		modelBatch = new AllModels(new ModelBuilder());
+		
+		Camera camera = new PerspectiveCamera(50, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		//Camera camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(0f, -20f, 0f);
+		//camera.near = 0.1f;
+		//camera.far = 30f;
+		
+		controller = new Controller(new Mouse(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), camera, modelBatch);
+		viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), controller.getCamera());
+		Gdx.input.setInputProcessor(controller);
+		
+		/*-----------------------------------------------------------------------------------------------------------*/
+		
+		modelBatch.addAtom(4,5,6, new Matrix4(new Vector3(0,0,0), new Quaternion(), new Vector3(1,1,1)), Color.RED, 40);
+	}
+	
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+	//	viewport.apply();
+		controller.updateCamera();
+		modelBatch.renderAll(controller.getCamera(), environment);
+	}
+	
+	@Override
+	public void pause() {
+		System.out.println("x0");
+	}
+	
+	@Override
+	public void resume() {
+		System.out.println("x1");
+	}
+	
+	@Override
+	public void dispose() {
+		System.out.println("x2");
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+		controller.resize(width, height);
+	}
+}
