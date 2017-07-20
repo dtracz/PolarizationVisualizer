cb9843777924127d957133e28e5dd2ad1f97e1aa
rzymianin4 <dave.x00@gmail.com>
2017-07-20 18:53:42 +0200

    gdx in swing subframes

--- desktop/src/com/visualizer/desktop/DesktopLauncher.java
+++ desktop/src/com/visualizer/desktop/DesktopLauncher.java
@@ -1,42 +1,11 @@
 package com.visualizer.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
-import com.visualizer.engine.Main;
+import com.visualizer.userInterface.ModelSubframe;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class DesktopLauncher {
-	public static class MyInternalFrame extends JInternalFrame {
-		static int openFrameCount = 0;
-		static final int xOffset = 30, yOffset = 30;
-		
-		public MyInternalFrame() {
-			super("Document #" + (++openFrameCount),
-					true, //resizable
-					true, //closable
-					true, //maximizable
-					true);//iconifiable
-			
-			//...Create the GUI and put it in the window...
-			JButton button2 = new JButton("load file2");
-			button2.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					System.out.println("dupa1");
-				}
-			});
-			button2.setBounds(100, 0, 100, 20);
-			//	add(button2);
-			
-			//...Then set the window size or call pack...
-			setSize(300, 300);
-			
-			//Set the window's location.
-			setLocation(xOffset * openFrameCount, yOffset * openFrameCount);
-		}
-	}
-	
+
 	public static void main(String[] arg) {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,16 +14,11 @@ public class DesktopLauncher {
 		frame.setContentPane(desktop);
 		desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
 		
-		MyInternalFrame inFrame = new MyInternalFrame();
-		inFrame.setVisible(true);
-		try {
-			inFrame.setSelected(true); }
-		catch(java.beans.PropertyVetoException e) { }
+		ModelSubframe subframe1 = new ModelSubframe("subframe_", 400, 300);
+		desktop.add(subframe1);
 		
-		desktop.add(inFrame);
-		
-		LwjglAWTCanvas canvas = new LwjglAWTCanvas(new Main());
-		inFrame.add(canvas.getCanvas());
+		ModelSubframe subframe2 = new ModelSubframe("subframe_", 400, 300);
+		desktop.add(subframe2);
 		
 		frame.setVisible(true);
 		frame.setSize(800, 600);
