6f3d486be61ee5be885a080816d385428e8edadd
rzymianin4 <dave.x00@gmail.com>
2017-07-20 17:42:40 +0200

    gdx in swing subframe

--- desktop/src/com/visualizer/desktop/DesktopLauncher.java
+++ desktop/src/com/visualizer/desktop/DesktopLauncher.java
@@ -1,93 +1,66 @@
 package com.visualizer.desktop;
 
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
-import com.visualizer.MainEngine;
-import org.lwjgl.Sys;
+import com.visualizer.engine.Main;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DesktopLauncher {
-	private static class ModelWindow extends JPanel {
-		public ModelWindow() {
-		}
-	}
-	
-	private static class MainWindow extends JFrame {
-		private JPanel panel;
-		private JButton button1, button2, button3;
+	public static class MyInternalFrame extends JInternalFrame {
+		static int openFrameCount = 0;
+		static final int xOffset = 30, yOffset = 30;
 		
-		public MainWindow() {
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		public MyInternalFrame() {
+			super("Document #" + (++openFrameCount),
+					true, //resizable
+					true, //closable
+					true, //maximizable
+					true);//iconifiable
 			
-			//final Container container = getContentPane();
-			//container.setLayout(new BorderLayout());
-			
-			/*-----------------------------------------------------------*/
-			
-			panel = new ModelWindow();
-			panel.setBounds(600,0, 200, 600);
-			add(panel);
-			
-			
-			button1 = new JButton( "load file1" );
-			button1.addActionListener( new ActionListener() {
+			//...Create the GUI and put it in the window...
+			JButton button2 = new JButton("load file2");
+			button2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("dupa1");
-				} } );
-			button1.setBounds(0, 0, 100, 20);
-			add(button1);
-			
-			button2 = new JButton( "load file2" );
-			button2.addActionListener( new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					System.out.println("dupa1");
-				} } );
+				}
+			});
 			button2.setBounds(100, 0, 100, 20);
-		//	add(button2);
+			//	add(button2);
 			
-			button3 = new JButton( "load file3" );
-			button3.addActionListener( new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					System.out.println("dupa1");
-				} } );
-			button3.setBounds(200, 0, 100, 20);
-		//	add(button3);
+			//...Then set the window size or call pack...
+			setSize(300, 300);
 			
-			/*-----------------------------------------------------------*/
-			
-			LwjglApplicationConfiguration config;
-			config = new LwjglApplicationConfiguration();
-			LwjglAWTCanvas canvas = new LwjglAWTCanvas(new MainEngine());
-			add(canvas.getCanvas(), BorderLayout.CENTER);
-			
-			
-			//pack();
-			setVisible(true);
-			setSize(800, 600);
-			
-			
-			
-			//setVisible(true);
+			//Set the window's location.
+			setLocation(xOffset * openFrameCount, yOffset * openFrameCount);
 		}
 	}
 	
-	public static void main (String[] arg) {
-		MainWindow window = new MainWindow();
-	/*	SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				MainWindow window = new MainWindow();
-			}
-		}); //*/
+	public static void main(String[] arg) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JDesktopPane desktop = new JDesktopPane();
+		frame.setContentPane(desktop);
+		desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+		
+		MyInternalFrame inFrame = new MyInternalFrame();
+		inFrame.setVisible(true);
+		try {
+			inFrame.setSelected(true); }
+		catch(java.beans.PropertyVetoException e) { }
+		
+		desktop.add(inFrame);
+		
+		LwjglAWTCanvas canvas = new LwjglAWTCanvas(new Main());
+		inFrame.add(canvas.getCanvas());
+		
+		frame.setVisible(true);
+		frame.setSize(800, 600);
+		
+		
 	}
-	
-//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		new LwjglApplication(new MyGdxGame(), config);
-//	}
 }
+
+
